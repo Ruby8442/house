24bbb82e19d93bb7227f93da84977b46d66aa083
Ruby8442 <ruby3810480@gmail.com>
2025-12-30 15:17:44 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,14 @@
-
-const CACHE_NAME = 'house-butler-v2';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
-];
-
-self.addEventListener('install', (e) => {
+self.addEventListener('install',e=>{
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+    caches.open('house-app').then(cache=>{
+      return cache.addAll(['index.html','manifest.json']);
     })
   );
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch',e=>{
   e.respondWith(
-    caches.match(e.request).then((res) => {
+    caches.match(e.request).then(res=>{
       return res || fetch(e.request);
     })
   );
